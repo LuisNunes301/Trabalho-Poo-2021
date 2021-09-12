d59f5386896bfe169af6e908a9f65803779c1d27
Luis Vinicius Nunes Silva <vnunes30@gmail.com>
2021-09-12 10:24:44 -0300

    Update VenderIngresso.java

--- poo-ap1/src/br/com/poo/modelo/VenderIngresso.java
+++ poo-ap1/src/br/com/poo/modelo/VenderIngresso.java
@@ -2,18 +2,19 @@ package br.com.poo.modelo;
 
 public class VenderIngresso {
 	/*
-	 * Classe primordial para a execução da venda de ingresso.
+	 * Classe primordial para a execuÃ§Ã£o da venda de ingresso.
 	 */
 	private double valorIngresso;
-
-	public VenderIngresso() {
-		this.valorIngresso = 0.00;
-	}
-
+	
+	
 	public VenderIngresso(double valorIngresso) {
 		this.valorIngresso = valorIngresso;
 	}
 
+	public VenderIngresso() {
+		this.valorIngresso = 0.00;
+	}
+	
 	/*
 	 * Metodo que seta a venda de Ingresso na Classe Shows.
 	 */
